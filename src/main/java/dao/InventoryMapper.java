176fbb3264b262feb2d09282a9189bcce8eccb67
Ryan Quey <quey.ryan@gmail.com>
2020-05-25 14:19:22 +0000

    create outline of dao implementation for podcasts

---
+++ src/main/java/dao/InventoryMapper.java
@@ -0,0 +1,33 @@
+package dao;
+
+import com.datastax.oss.driver.api.mapper.annotations.DaoFactory;
+import com.datastax.oss.driver.api.mapper.annotations.DaoKeyspace;
+import com.datastax.oss.driver.api.mapper.annotations.Mapper;
+
+/*
+ *
+ * https://github.com/datastax/java-driver/tree/4.x/manual/mapper
+ *
+ * How to get: 
+ *  InventoryMapper inventoryMapper = InventoryMapper.builder(session).build();
+ *  PodcastDao dao = inventoryMapper.podcastDao("podcast_analysis_tool", "podcasts_by_language");
+ *  dao.findById("this-is-the-podcast-id");
+ *
+ *
+ * Note that "The mapper maintains an interface cache. Calling a factory method with the same arguments will yield the same DAO instance"
+ *
+ *
+ *
+ */
+@Mapper
+public interface InventoryMapper {
+  @DaoFactory
+  PodcastDao podcastDao(@DaoKeyspace CqlIdentifier keyspace);
+
+  // helper so can use the inventoryMapper more easily.
+  // https://github.com/datastax/java-driver/tree/4.x/manual/mapper/mapper#mapper-builder
+  static MapperBuilder<InventoryMapper> builder(CqlSession session) {
+    return new InventoryMapperBuilder(session);
+  }
+
+}
---
+++ src/main/java/dao/PodcastDao.java
@@ -0,0 +1,61 @@
+package dao;
+
+import com.datastax.oss.driver.api.core.PagingIterable;
+import com.datastax.oss.driver.api.mapper.annotations.Dao;
+import com.datastax.oss.driver.api.mapper.annotations.QueryProvider;
+import com.datastax.oss.driver.api.mapper.annotations.Select;
+import com.datastax.oss.driver.api.mapper.annotations.Update;
+import com.datastax.oss.driver.api.mapper.entity.saving.NullSavingStrategy;
+import java.util.UUID;
+
+import dataClasses.Podcast;
+
+@Dao
+public interface PodcastDao {
+
+  /** Simple selection by full primary key. */
+  @Select
+  Podcast findOne(String language, String primaryGenre, String feedUrl);
+
+  /**
+   * Selection by partial primary key, this will return multiple rows.
+   *
+   * <p>Also, note that this queries a different table: DAOs are not limited to a single entity, the
+   * return type of the method dictates what rows will be mapped to.
+   */
+	/*
+  @Select
+  PagingIterable<PodcastByLanguage> getByLanguage(String language);
+  @Select
+  PagingIterable<PodcastByPrimaryGenre> getByLanguage(String language, String primaryGenre);
+	*/
+
+  /**
+   * Creating a video is a bit more complex: because of denormalization, it involves multiple
+   * tables.
+   *
+   * <p>A query provider is a nice way to wrap all the queries in a single operation, and hide the
+   * details from the DAO interface.
+   */
+  // not doing for now, only doing one table
+  /*
+  @QueryProvider(
+      providerClass = CreatePodcastQueryProvider.class,
+      entityHelpers = {Podcast.class, UserPodcast.class, LatestPodcast.class, PodcastByTag.class})
+  void create(Podcast video);
+  */
+  @Insert
+  void create(Podcast podcast);
+
+  /**
+   * Update using a template: the template must have its full primary key set; beyond that, any
+   * non-null field will be considered as a value to SET on the target row.
+   *
+   * <p>Note that we specify the null saving strategy for emphasis, but this is the default.
+   *
+   * For more on how to do customWhereClause and other options, see here: https://github.com/datastax/java-driver/tree/4.x/manual/mapper/daos/update#parameters
+   * For our use case allowing three languages so can do e.g., dao.update(podcast, en, en-US, and UNKNOWN);
+   */
+  @Update(customWhereClause = "language IN (:language1, :language2, :language3)", nullSavingStrategy = NullSavingStrategy.DO_NOT_SET)
+  void update(Podcast podcast, String language1, String language2);
+}
