ccb2098966bf3b0cca868688731962fbdfa73b7f
Ryan Quey <quey.ryan@gmail.com>
2020-07-10 06:14:38 +0000

    change the hardcoded *ahem* path to kafka

--- scripts/kafka-testers/start-search-results-json-consumer.sh
+++ scripts/kafka-testers/start-search-results-json-consumer.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -eux
 
 # consumes what our kafka streams word count stuff produces
-cd $HOME/kafka_2.12-2.5.0
+cd $HOME/lib/kafka_2.12-2.5.0
 echo "Starting consumer for queue.podcast-analysis-tool.search-results-json"
 # see here for how to set up the deserializer in cli 
 # http://mail-archives.apache.org/mod_mbox/kafka-users/201709.mbox/%3CCAMdhPwRYRBg-LKbS_z3Ar5m0BkpLU8=gFR1E=yyh-z_Psa3s1g@mail.gmail.com%3E
-$HOME/kafka_2.12-2.5.0/bin/kafka-console-consumer.sh --bootstrap-server localhost:9092 --topic queue.podcast-analysis-tool.search-results-json --from-beginning \
+$HOME/lib/kafka_2.12-2.5.0/bin/kafka-console-consumer.sh --bootstrap-server localhost:9092 --topic queue.podcast-analysis-tool.search-results-json --from-beginning \
     --property print.key=true \
     --property print.value=true \
     --property key.deserializer=org.apache.kafka.common.serialization.StringDeserializer \
--- scripts/kafka-testers/start-search-term-producer.sh
+++ scripts/kafka-testers/start-search-term-producer.sh
@@ -1,5 +1,5 @@
 # start producing. Standalone thing to run in separate terminal to run against run-search-per-term-${project-package.version}.jar
 echo "Starting producer for queue.podcast-analysis-tool.query-term"
-$HOME/kafka_2.12-2.5.0/bin/kafka-console-producer.sh --bootstrap-server localhost:9092 --topic queue.podcast-analysis-tool.query-term
+$HOME/lib/kafka_2.12-2.5.0/bin/kafka-console-producer.sh --bootstrap-server localhost:9092 --topic queue.podcast-analysis-tool.query-term
 
 
